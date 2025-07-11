dfb611a5bab3bb0f93e050df55d8dd60572a3b87
Your Name <lhunyadi@outlook.com>
2025-07-11 17:22:42 +0200

    GitHub Pages - Correction

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/modern-calendar-app/' : '/',
+  base: command === 'build' ? '/calendar/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
